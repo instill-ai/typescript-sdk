fb670b7a82597be51862492501c784b170dbde80
Po Chun Chiu <57251712+EiffelFly@users.noreply.github.com>
2024-05-07 16:02:30 +0800

    fix(deps): further fix deps error (#77)
    Because
    
    - There are lots of deps alert due to old and not up-to-date deps. 
    
    This commit
    
    - further fix deps error

--- setupTests.ts
+++ setupTests.ts
@@ -1,5 +1,1 @@
-import { expect } from "vitest";
-import matchers from "@testing-library/jest-dom/matchers";
-
-// extends Vitest's expect method with methods from react-testing-library
-expect.extend(matchers);
+import "@testing-library/jest-dom/vitest";
